da88c713773edc3168b22dc4b5d517cc6bd36797
corytimmons1688 <ctimmons@calyxcontainers.com>
2026-01-16 10:49:43 -0700

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Utils package for project management tool.
-Contains data management and helper functions.
-"""
-
-from .data_manager import DataManager, generate_project_id, generate_subtask_id
-
-__all__ = ['DataManager', 'generate_project_id', 'generate_subtask_id']
